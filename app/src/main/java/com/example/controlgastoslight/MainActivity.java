bbf23342e8814300085f290f2e5c39bb30775f6e
alejandroat99 <alejandroat99@uma.es>
2021-01-03 12:18:13 +0100

    Creada barra de navegación entre pantallas principales (home, grupos y estadisticas) de la aplicación. Referencia: https://www.youtube.com/watch?v=Chso6xrJ6aU

--- app/src/main/java/com/example/controlgastoslight/MainActivity.java
+++ app/src/main/java/com/example/controlgastoslight/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.controlgastoslight;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHostController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation_view);
+        NavHostFragment hostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.host_fragment);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.homeFragment, R.id.gruposFragment, R.id.estadisticasFragment
+        ).build();
+
+        NavigationUI.setupActionBarWithNavController(this, hostFragment.getNavController(), appBarConfiguration);
+
+        NavigationUI.setupWithNavController(bottomNavigationView, hostFragment.getNavController());
+
     }
 }
