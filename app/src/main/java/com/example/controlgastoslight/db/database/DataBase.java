298583d5348151b1d79ef6084c9da32e1de17a9a
pepedg <pepediazgaspar@gmail.com>
2021-01-05 01:23:19 +0100

    La base de datos ahora usa el patrón Singleton.

--- app/src/main/java/com/example/controlgastoslight/db/database/DataBase.java
+++ app/src/main/java/com/example/controlgastoslight/db/database/DataBase.java
@@ -1,6 +1,9 @@
 package com.example.controlgastoslight.db.database;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import com.example.controlgastoslight.db.dao.GrupoDao;
@@ -13,7 +16,23 @@ import com.example.controlgastoslight.db.model.RegistroGrupoCrossRef;
 
 @Database(entities = {Registro.class, Grupo.class, RegistroGrupoCrossRef.class}, version = 1)
 public abstract class DataBase extends RoomDatabase {
+    // Singleton patron
+    private static DataBase INSTANCE;
+
     public abstract RegistroDao registroDao();
     public abstract GrupoDao grupoDao();
     public abstract RegistroGrupoCrossRefDao registroGrupoCrossRefDao();
+
+    public static synchronized DataBase getInMemoryDatabase(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE =
+                    Room.inMemoryDatabaseBuilder(context.getApplicationContext(), DataBase.class).allowMainThreadQueries().
+                            fallbackToDestructiveMigration().build();
+        }
+        return INSTANCE;
+    }
+
+    public static void destroyInstance() {
+        INSTANCE = null;
+    }
 }
